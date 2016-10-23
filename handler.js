b8d8274937438c8ac177ed27c06f408fab47d275
wei <wrenth04@gmail.com>
2016-10-23 21:54:13 +0800

    chore(aws): setup lambda schedual

--- handler.js
+++ handler.js
@@ -1,16 +1,9 @@
-'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
+const _pttbot = require('./pttbot');
+
+module.exports.pttbot = (event, context, callback) => {
+  _pttbot((err, res) => callback(null, {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    body: JSON.stringify(res),
+  }));
 };
---
+++ pttbot.js
@@ -0,0 +1,6 @@
+
+module.exports = (callback) => {
+  console.log('in pttbot.js');
+  callback();
+}
+
