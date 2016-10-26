c935fb6c49c1050177c4b39b9bf6521e05730f05
wei <wrenth04@gmail.com>
2016-10-26 11:58:42 +0800

    feat(db): add firebase db

--- pttbot.js
+++ pttbot.js
@@ -4,8 +4,16 @@ var moment = require('moment');
 var request = require('request');
 var config = require('./config');
 
+var server = firebase.initializeApp({
+  serviceAccount: config.SERVICE_ACCOUNT,
+  databaseURL: config.DB_URI
+}, 'server');
+
+var root = server.database();
+
 module.exports = (callback) => {
-  console.log('in pttbot.js');
   callback();
 }
+
+
 
